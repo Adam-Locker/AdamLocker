dd73ccc21b9dcc01ecfcb5b67108395e2b0761b0
unknown <Adam.Locker@S3242.hthla.org>
2015-09-29 14:49:34 -0700

    Updated intitialize and execute

--- src/org/usfirst/frc/team4/robot/commands/compressorStart.java
+++ src/org/usfirst/frc/team4/robot/commands/compressorStart.java
@@ -12,11 +12,11 @@ public class compressorStart extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	Robot.pneumatics.compressorStart();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.pneumatics.compressorStart();
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- src/org/usfirst/frc/team4/robot/commands/compressorStop.java
+++ src/org/usfirst/frc/team4/robot/commands/compressorStop.java
@@ -12,11 +12,11 @@ public class compressorStop extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	Robot.pneumatics.compressorStop();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.pneumatics.compressorStop();
     }
 
     // Make this return true when this Command no longer needs to run execute()
